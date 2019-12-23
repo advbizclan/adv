274722bcc3bedc1a8f89095603ee523c837facde
anasakhtar01 <anas.akhtar01@gmail.com>
2019-12-23 19:57:05 +0500

    Updates

---
+++ precache-manifest.013e023731248f9d1c14f9e9d4618c94.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/adv/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "df0ce06f40f74fd3b610",
+    "url": "/adv/static/js/runtime~main.df0ce06f.js"
+  },
+  {
+    "revision": "a7f6bd603f7004b96562",
+    "url": "/adv/static/js/main.a7f6bd60.chunk.js"
+  },
+  {
+    "revision": "630e7d1ee6645fd40b29",
+    "url": "/adv/static/js/1.630e7d1e.chunk.js"
+  },
+  {
+    "revision": "a7f6bd603f7004b96562",
+    "url": "/adv/static/css/main.f0a57376.chunk.css"
+  },
+  {
+    "revision": "07bca28b4a1c0d5b300ed66537acde58",
+    "url": "/adv/index.html"
+  }
+];
